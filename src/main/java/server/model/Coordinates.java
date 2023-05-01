d41a2d6f82c32b10e9be9d2582a817718e7a57c1
Ed <eduard.topalov@yandex.ru>
2023-05-01 17:09:41 +0300

    Added model

---
+++ src/main/java/server/model/Coordinates.java
@@ -0,0 +1,35 @@
+package server.model;
+
+public class Coordinates {
+    private int x;
+    private Integer y; //Значение поля должно быть больше -775, Поле не может быть null
+
+    public Coordinates(int x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
---
+++ src/main/java/server/model/Vehicle.java
@@ -0,0 +1,116 @@
+package server.model;
+
+import java.time.LocalDate;
+
+public class Vehicle {
+    private Integer id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
+    private java.time.LocalDate creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    private int enginePower; //Значение поля должно быть больше 0
+    private Float capacity; //Поле может быть null, Значение поля должно быть больше 0
+    private Float fuelConsumption; //Поле не может быть null, Значение поля должно быть больше 0
+    private FuelType fuelType; //Поле может быть null
+
+    public Vehicle(Integer id, String name, Coordinates coordinates, LocalDate creationDate, int enginePower, Float capacity, Float fuelConsumption, FuelType fuelType) {
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate;
+        this.enginePower = enginePower;
+        this.capacity = capacity;
+        this.fuelConsumption = fuelConsumption;
+        this.fuelType = fuelType;
+    }
+
+    public Vehicle(String name, Coordinates coordinates, LocalDate creationDate, int enginePower, Float capacity, Float fuelConsumption, FuelType fuelType) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate;
+        this.enginePower = enginePower;
+        this.capacity = capacity;
+        this.fuelConsumption = fuelConsumption;
+        this.fuelType = fuelType;
+    }
+
+    public Vehicle() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getEnginePower() {
+        return enginePower;
+    }
+
+    public void setEnginePower(int enginePower) {
+        this.enginePower = enginePower;
+    }
+
+    public Float getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Float capacity) {
+        this.capacity = capacity;
+    }
+
+    public Float getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(Float fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", coordinates=" + coordinates +
+                ", creationDate=" + creationDate +
+                ", enginePower=" + enginePower +
+                ", capacity=" + capacity +
+                ", fuelConsumption=" + fuelConsumption +
+                ", fuelType=" + fuelType +
+                '}';
+    }
+}
